90b2a8e539d919d340391db68a74e54112152afe
Nobeul <nobeul.cse@gmail.com>
2020-07-13 02:02:44 +0600

    updated relationship between product model and review model

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+        return $this->hasMany(Review::class, 'product_id', 'id');
+    }
 }
