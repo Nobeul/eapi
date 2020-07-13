cc26b8ba8959201479cc673b59953f22e86312ad
Nobeul <nobeul.cse@gmail.com>
2020-07-14 02:07:05 +0600

    Updated ProductCollection and changed it to Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -3,9 +3,9 @@
 namespace App\Http\Resources\Product;
 
 use App\Model\Product;
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -15,6 +15,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'price' => $this->price,
+            'discount' => $this->discount,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating available yet!',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
+        ];
     }
 }
