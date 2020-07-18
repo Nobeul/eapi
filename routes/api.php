b3e8efaf8c6c10f3ef5abde7ed8af7d52c1d725d
Nobeul <nobeul.cse@gmail.com>
2020-07-19 01:45:56 +0600

    Product creation with validation and authentication is done

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // This route is to access authenticated user
-Route::middleware('auth:api')->get('/user',function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 // This route is to get all products
